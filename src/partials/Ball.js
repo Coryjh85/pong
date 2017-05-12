570c8c37094904620a98eb962455cd95d175cbcd
Cory <Coryjh85@gmail.com>
2017-05-12 11:02:48 -0700

    wall collision works

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -6,13 +6,44 @@ export default class Ball {
     this.boardWidth = boardWidth;
     this.boardHeight = boardHeight;
     this.radius = radius;
+    this.direction = 1;
     this.reset();
   }
-reset() {
-  this.x = this.boardWidth / 2;
-  this.y = this.boardHeight / 2;
+  reset() {
+    this.x = this.boardWidth / 2;
+    this.y = this.boardHeight / 2;
+
+    //generate a random # between -5 and 5 but not 0
+    this.vy = 0;
+    while (this.vy === 0) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
+    }
+
+    // also a number between -5 and 5 based on the vy
+    this.vx = this.direction * (6 - Math.abs(this.vy));
+  }
+
+  wallCollision() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+
+  if(hitLeft || hitRight){
+    this.vx = -this.vx;
+  }
+
+  if(hitTop || hitBottom){
+    this.vy = -this.vy;
+  }
 }
+
   render(svg) {
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.wallCollision();
 
     let ball = document.createElementNS(SVG_NS, 'circle');
     ball.setAttributeNS(null, 'cx', this.x);
